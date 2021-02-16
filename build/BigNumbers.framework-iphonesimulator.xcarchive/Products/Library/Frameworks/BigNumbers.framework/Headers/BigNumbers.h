c457fe5abfa2a050ed7159cf5abd80c388bfcb44
Marek Piotrowski-EXT <Marek.Piotrowski@jeppesen.com>
2021-02-16 16:06:45 +0100

    frameworks 3

---
+++ build/BigNumbers.framework-iphonesimulator.xcarchive/Products/Library/Frameworks/BigNumbers.framework/Headers/BigNumbers.h
@@ -0,0 +1,19 @@
+//
+//  BigNumbers.h
+//  BigNumbers
+//
+//  Created by Marek Piotrowski-EXT on 15/02/2021.
+//  Copyright © 2021 Marek Piotrowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BigNumbers.
+FOUNDATION_EXPORT double BigNumbersVersionNumber;
+
+//! Project version string for BigNumbers.
+FOUNDATION_EXPORT const unsigned char BigNumbersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BigNumbers/PublicHeader.h>
+
+
